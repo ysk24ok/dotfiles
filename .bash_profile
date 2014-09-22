4091c7384a40224e9cbc7a112a0f6cfd25433b22
Yusuke NISHIOKA <maximilianthermidor765@gmail.com>
2014-09-22 11:46:20 +0900

    Add bash settings

--- .bash_profile
+++ .bash_profile
@@ -3,6 +3,10 @@ if [ -f ~/.bashrc ] ; then
   . ~/.bashrc
 fi
 
+# editor setting
+export BUNDLER_EDITOR=vim
+export EDITOR=vim
+
 # terminal color setting
 export CLICOLOR=1
 export LSCOLORS=DxGxcxdxCxegedabagacad
@@ -12,3 +16,10 @@ export TERM=xterm-256color
 export CC=gcc
 export CXX=g++
 export CPPFLAGS="-march=core2 -mtune=core2"
+
+if [ `uname` = "Darwin" ]; then
+  source ~/dotfiles/.bash_profile_mac
+  echo " - reloaded .bash_profile for Mac"
+elif [ `uname` = "Linux" ]; then
+  echo " - reloaded .bash_profile for Linux"
+fi
--- post-merge.sh
+++ post-merge.sh
@@ -2,18 +2,31 @@
 
 echo "Now executing post-merge hook ..."
 
+# bash setting
+cp ./.bash_profile_global ~/.bash_profile
+cp ./.bashrc_global ~/.bashrc
+source ~/.bashrc
+source ~/.bash_profile
+echo " - copied and reloaded .bashrc and .bash_profile"
+
 # git setting
 cp ./.gitconfig_global ~/.gitconfig
 cp ./.gitignore_global ~/.gitignore_global
+echo " - copied .gitconfig and .gitignore"
 
-# vim seting
+# vim setting
 if [ ! -e ~/.vim ]; then
   mkdir -p ~/.vim/bundle
   git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
+  echo " - cloned Shougo/neobundle.vim"
+
+  git clone git://github.com/Shougo/vimproc ~/.vim/bundle/vimproc
+  cd ~/.vim/bundle/vimproc && make && cd ~/dotfiles
+  echo " - cloned Shougo/vimproc and executed make"
 fi
 
-vim ./vimrc
 cp ./.vimrc_global ~/.vimrc
-source ~/.vimrc
+vim ~/.vimrc    # run :NeoBundleInstall here and reload
+echo " - reloaded .vimrc"
 
 echo "done!"
