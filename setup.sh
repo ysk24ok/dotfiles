d90b6252aacba65561d2bd524e7ffe39c2e7fbda
Yusuke Nishioka <yusuke.nishioka.0713@gmail.com>
2021-08-03 20:52:08 +0900

    Refactor setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DOTFILES_DIR="$(realpath $0)"
+DOTFILES_DIR=$(dirname $(realpath $0))
 
 function check_command() {
   if ! type $1 > /dev/null 2>&1; then
@@ -9,78 +9,57 @@ function check_command() {
   fi
 }
 
-function main() {
-  echo 'Now executing setup script ...'
-
-  # git
-  if [ ! -e ~/.gitconfig ]; then
-    cp $DOTFILES_DIR/gitconfig ~/.gitconfig
-    echo ' - gitconfig is copied'
+function setup_file() {
+  file=$1
+  dest=~/.$1
+  if [ ! -e $dest ]; then
+    cp $DOTFILES_DIR/$file $dest
+    echo $dest is copied.
   else
-    echo ' - gitconfig already exists'
+    echo $dest already exists.
   fi
-  if [ ! -e ~/.gitignore_global ]; then
-    cp $DOTFILES_DIR/gitignore_global ~/.gitignore_global
-    echo ' - gitignore_global is copied'
-  else
-    echo ' - gitignore_global already exists'
-  fi
+}
 
-  # screenrc
-  if [ ! -e ~/.screenrc ]; then
-    cp $DOTFILES_DIR/screenrc ~/.screenrc
-    echo ' - screenrc is copied'
+function setup_shell_file() {
+  file=$1
+  dest=~/.$1
+  if [ ! -e $dest ]; then
+    echo source $DOTFILES_DIR/$file > $dest
+    source $dest
+    echo $dest is created.
   else
-    echo ' - screenrc already exists'
+    echo $dest already exists.
   fi
+}
 
-  # tmux
-  if [ ! -e ~/.tmux.conf ]; then
-    cp $DOTFILES_DIR/tmux.conf ~/.tmux.conf
-    echo ' - tmux.conf is copied'
-  else
-    echo ' - tmux.conf already exists'
-  fi
-
+function setup_vim() {
   # vim-plug
   if [ ! -e ~/.vim/autoload/plug.vim ]; then
     curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
     echo ' - vim-plug is installed'
   fi
-  # .vimrc
-  if [ ! -e ~/.vimrc ]; then
-    echo "source $DOTFILES_DIR/vimrc" > ~/.vimrc
-    echo ' - ~/.vimrc is set'
-  else
-    echo ' - ~/.vimrc already exists'
-  fi
+  setup_file vimrc
   vim ~/.vimrc    # run :PlugInstall here and reload
-  echo ' - reloaded .vimrc'
+}
 
-  # bash
-  if [ ! -e ~/.bashrc ]; then
-    echo "source $DOTFILES_DIR/bashrc" > ~/.bashrc
-    echo ' - ~/.bashrc is copied'
-  else
-    echo ' - ~/.bashrc already exists'
-  fi
-  if [ ! -e ~/.bash_profile ]; then
-    echo "source $DOTFILES_DIR/bash_profile" > ~/.bash_profile
-    echo ' - ~/.bash_profile is copied'
-  else
-    echo ' - ~/.bash_profile already exists'
-  fi
-  if [ ! -e ~/.bash_logout ]; then
-    echo "source $DOTFILES_DIR/bash_logout" > ~/.bash_logout
-    echo ' - ~/.bash_logout is copied'
-  else
-    echo ' - ~/.bash_logout already exists'
-  fi
-  source ~/.bashrc
-  source ~/.bash_profile
-  source ~/.bash_logout
-  echo ' - reloaded .bashrc, .bash_profile and .bash_logout'
+function setup_bash() {
+  setup_shell_file bashrc
+  setup_shell_file bash_profile
+  setup_shell_file bash_logout
+}
+
+function main() {
+  echo 'Now executing setup script ...'
+
+  setup_file gitconfig
+  setup_file gitignore_global
+  setup_file screenrc
+  setup_file tmux.conf
+
+  setup_vim
+
+  setup_bash
 
   echo 'done!'
 }
